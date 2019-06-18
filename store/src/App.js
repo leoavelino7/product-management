6bc9dbd8e4234d516e6f3e0004b2505ffe59256c
leoavelino7 <leonardo.avelino16@hotmail.com>
2019-06-18 14:40:59 -0300

    Adding Code splitting and Lazy loading

--- store/src/App.js
+++ store/src/App.js
@@ -1,33 +1,39 @@
-import React from 'react';
+import React, { Component, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
-import NewProductView from "./views/NewProductView";
-import ProductListView from "./views/ProductListView";
+const NewProductView = React.lazy( () => import("./views/NewProductView"));
+const ProductListView = React.lazy( () => import( "./views/ProductListView"));
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <div>
-          <header>
-            <ul className="link-list">
-              <li>
-                <Link to={"/"}>Novo</Link>
-              </li>
-              <li>
-                <Link to={"/list"}>Lista</Link>
-              </li>
-            </ul>
-          </header>
+
+class App extends Component {
+  render() {
+
+    return (
+      <div className="App">
+        <Router>
           <div>
-            <Route path={"/"} exact component={NewProductView} />
-            <Route path={"/list"} component={ProductListView} />
+            <header>
+              <ul className="link-list">
+                <li>
+                  <Link to={"/"}>Novo</Link>
+                </li>
+                <li>
+                  <Link to={"/list"}>Lista</Link>
+                </li>
+              </ul>
+            </header>
+            <div>
+              <Suspense fallback={<div>Carregando...</div>}>
+                <Route path={"/"} exact component={(props) => <NewProductView {...props} />} />
+                <Route path={"/list"} component={(props) => <ProductListView {...props} />} />
+              </Suspense>
+            </div>
           </div>
-        </div>
-      </Router>
-    </div>
-  );
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
