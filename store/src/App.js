75b5b13cc6adefe4a373629fbf43c7799900531f
leoavelino7 <leonardo.avelino16@hotmail.com>
2019-06-18 13:58:40 -0300

    Adjusting product list

--- store/src/App.js
+++ store/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import NewProductView from "./views/NewProductView";
+import ProductListView from "./views/ProductListView";
 
 function App() {
   return (
     <div className="App">
       <NewProductView />
+      <ProductListView />
     </div>
   );
 }
